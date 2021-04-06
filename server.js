a55554697a9cb588fe05ffa8fc0e14d398ae2974
Alfonso Villanueva <avillanMty01@yahoo.com>
2021-04-06 13:26:42 -0500

    Server setup complete
    basic express server up and mongodb connection ready

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config({path:'.env'});
+}
+
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
@@ -10,6 +14,11 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('alf: Mongoose connection successful'))
 app.use('/', indexRouter)
 
 app.listen(process.env.PORT || 3000)
