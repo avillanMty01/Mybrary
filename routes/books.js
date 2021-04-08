f3047fd580c5b997c9ab2bdbb40e82d620d3612e
Alfonso Villanueva <avillanMty01@yahoo.com>
2021-04-08 13:39:20 -0500

    going to branch books

---
+++ routes/books.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+const Author = require('../models/book')
+
+// All books route
+router.get('/', async (req, res) => {
+    
+})
+
+// New book route
+router.get('/new', (req, res) => {
+    
+})
+
+// Create author route
+router.post('/', async (req, res) => {
+  
+})
+
+
+module.exports = router
